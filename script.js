9384320d4f1f1e8b0a77f5258cd1e2fbc17c040c
arsadssss <arsadsagir@gmail.com>
2024-09-25 00:21:28 +0500

    Product Page added

--- script.js
+++ script.js
@@ -34,6 +34,40 @@ function cartNumber(){
     cart++;
     cartCount.textContent = cart;
 }
+
 addtocart.addEventListener("click", function(){
     cartNumber();
+});
+
+const orders = document.querySelectorAll(".orders");
+
+
+
+orders.forEach((orders)=>{
+    orders.addEventListener("click", function(event){
+        const orderItem = event.target.closest('.order-items');
+        if(orderItem){
+            var productImg = orderItem.querySelector('img').src;
+            var productTitle = orderItem.querySelector('.title').textContent || "";
+            var productSubtitle;
+            // var productSubtitle = orderItem.querySelector('.subtitle').textContent || "";
+            if(orderItem.querySelector('.subtitle')){
+                productSubtitle = orderItem.querySelector('.subtitle').textContent;
+            }else{
+                productSubtitle = "Brand name not registered yet..";
+            }
+            var productPrice = orderItem.querySelector('.price').textContent || "";
+            
+            const params = new URLSearchParams({
+                productImg : productImg,
+                productSubtitle : productSubtitle,
+                productTitle : productTitle,
+                productPrice : productPrice
+            });
+            window.location.href = `product.html?${params.toString()}`;
+        }else{
+            console.log("No Item found");
+        }
+    });
 })
+
---
+++ scriptProduct.js
@@ -0,0 +1,49 @@
+var productName = document.querySelectorAll(".productName");
+var brandName = document.querySelector("#brandName");
+var productImg = document.querySelector(".productImg");
+var finalPrice = document.querySelector(".finalPrice");
+
+const params = new URLSearchParams(window.location.search);
+
+productImg.src = params.get('productImg');
+brandName.textContent = params.get('productSubtitle');
+productName.forEach((words)=>{
+    words.textContent = params.get('productTitle');
+})
+finalPrice.textContent = params.get('productPrice');
+newPrice = params.get('productPrice');
+
+
+var quantity = document.querySelector("#quantity");
+var sub = document.querySelector("#sub");
+var add = document.querySelector("#add");
+let quan = 1;
+
+add.addEventListener("click", function(){
+    quan++;
+    quantity.textContent = quan;
+    finalPrice.textContent = quan*Number(newPrice);
+    if(quan > 2){
+        document.querySelector(".offerPrice").style.textDecoration = "line-through";
+    }else{
+        document.querySelector(".offerPrice").style.textDecoration = "none";
+    }
+});
+    sub.addEventListener("click", function(){
+        if(quan > 1){
+            quan--;
+            finalPrice.textContent = (Number(finalPrice.textContent) - newPrice).toFixed(2);
+            quantity.textContent = quan;
+        }else{
+            quan = 1;
+        }
+    });
+
+    const cartCount = document.querySelector("#cartCount");
+    const addToCart = document.querySelector("#addToCart");
+    
+    // let cart = 0;
+    
+    addToCart.addEventListener("click", function(){
+        cartCount.textContent = quan;
+    });
